e6181c86ec0d4168653f3ea603ae9c8412f43611
abokareem <elnwajha.pal@gmail.com>
2020-02-17 12:41:08 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,6 +2,24 @@ sudo apt-get update
 
 sudo apt-get install xrdp
 
+
+sudo apt-get -y install xrdp
+
+sudo nano /etc/xrdp/xrdp.ini
+
+encrypt_level=high
+
+sudo nano /etc/polkit-1/localauthority.conf.d/02-allow-colord.conf
+polkit.addRule(function(action, subject) {
+if ((action.id == “org.freedesktop.color-manager.create-device” || action.id == “org.freedesktop.color-manager.create-profile” || action.id == “org.freedesktop.color-manager.delete-device” || action.id == “org.freedesktop.color-manager.delete-profile” || action.id == “org.freedesktop.color-manager.modify-device” || action.id == “org.freedesktop.color-manager.modify-profile”) && subject.isInGroup(“{group}”))
+{
+return polkit.Result.YES;
+}
+});
+
+
+sudo /etc/init.d/xrdp restart
+
 sudo apt-get install xfce4
 
 للدخول عبر سطح مكتب بعيد
